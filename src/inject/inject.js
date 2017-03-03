583f0499333c20ba6510659ef203a4da6850fa86
Matthew Salcido <matthew.salcido@healthsparq.com>
2017-03-02 20:20:48 -0800

    Move label xml into inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,154 @@
 // ========================================================
+// Label Template XML
+// ========================================================
+
+let template = '<?xml version="1.0" encoding="utf-8"?>\
+									<DieCutLabel Version="8.0" Units="twips">\
+										<PaperOrientation>Landscape</PaperOrientation>\
+										<Id>Shipping</Id>\
+										<PaperName>30323 Shipping</PaperName>\
+										<DrawCommands>\
+											<RoundRectangle X="0" Y="0" Width="3060" Height="5715" Rx="270" Ry="270"/>\
+										</DrawCommands>\
+										<ObjectInfo>\
+											<TextObject>\
+												<Name>ARTIST</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>True</IsVariable>\
+												<HorizontalAlignment>Left</HorizontalAlignment>\
+												<VerticalAlignment>Top</VerticalAlignment>\
+												<TextFitMode>None</TextFitMode>\
+												<UseFullFontHeight>True</UseFullFontHeight>\
+												<Verticalized>False</Verticalized>\
+												<StyledText>\
+													<Element>\
+														<String>Artist Title</String>\
+														<Attributes>\
+															<Font Family="Helvetica" Size="12" Bold="True" Italic="False" Underline="False" Strikeout="False"/>\
+															<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+														</Attributes>\
+													</Element>\
+												</StyledText>\
+											</TextObject>\
+											<Bounds X="307.1997" Y="73.64145" Width="5323.2" Height="328.5938"/>\
+										</ObjectInfo>\
+										<ObjectInfo>\
+											<TextObject>\
+												<Name>TITLE</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>True</IsVariable>\
+												<HorizontalAlignment>Left</HorizontalAlignment>\
+												<VerticalAlignment>Top</VerticalAlignment>\
+												<TextFitMode>None</TextFitMode>\
+												<UseFullFontHeight>True</UseFullFontHeight>\
+												<Verticalized>False</Verticalized>\
+												<StyledText>\
+													<Element>\
+														<String>Release title</String>\
+														<Attributes>\
+															<Font Family="Helvetica" Size="9" Bold="False" Italic="False" Underline="False" Strikeout="False"/>\
+															<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+														</Attributes>\
+													</Element>\
+												</StyledText>\
+											</TextObject>\
+											<Bounds X="307.1997" Y="376.9271" Width="5323.2" Height="223.2292"/>\
+										</ObjectInfo>\
+										<ObjectInfo>\
+											<TextObject>\
+												<Name>GENRE</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>True</IsVariable>\
+												<HorizontalAlignment>Left</HorizontalAlignment>\
+												<VerticalAlignment>Top</VerticalAlignment>\
+												<TextFitMode>None</TextFitMode>\
+												<UseFullFontHeight>True</UseFullFontHeight>\
+												<Verticalized>False</Verticalized>\
+												<StyledText>\
+													<Element>\
+														<String>Genre</String>\
+														<Attributes>\
+															<Font Family="Helvetica" Size="10" Bold="True" Italic="False" Underline="False" Strikeout="False"/>\
+															<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+														</Attributes>\
+													</Element>\
+												</StyledText>\
+											</TextObject>\
+											<Bounds X="316.1458" Y="761.3541" Width="5323.2" Height="337.7604"/>\
+										</ObjectInfo>\
+										<ObjectInfo>\
+											<TextObject>\
+												<Name>NOTES</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>True</IsVariable>\
+												<HorizontalAlignment>Left</HorizontalAlignment>\
+												<VerticalAlignment>Top</VerticalAlignment>\
+												<TextFitMode>None</TextFitMode>\
+												<UseFullFontHeight>True</UseFullFontHeight>\
+												<Verticalized>False</Verticalized>\
+												<StyledText>\
+													<Element>\
+														<String>Release notes</String>\
+														<Attributes>\
+															<Font Family="Helvetica" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False"/>\
+															<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+														</Attributes>\
+													</Element>\
+												</StyledText>\
+											</TextObject>\
+											<Bounds X="307.1997" Y="1199.789" Width="5323.2" Height="2000"/>\
+										</ObjectInfo>\
+										<ObjectInfo>\
+											<ShapeObject>\
+												<Name>SHAPE</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>False</IsVariable>\
+												<ShapeType>HorizontalLine</ShapeType>\
+												<LineWidth>20</LineWidth>\
+												<LineAlignment>LeftOrTop</LineAlignment>\
+												<FillColor Alpha="0" Red="0" Green="0" Blue="0"/>\
+											</ShapeObject>\
+											<Bounds X="307.1997" Y="652.9683" Width="5323.2" Height="94.42623"/>\
+										</ObjectInfo>\
+										<ObjectInfo>\
+											<ShapeObject>\
+												<Name>SHAPE_1</Name>\
+												<ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
+												<BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
+												<LinkedObjectName></LinkedObjectName>\
+												<Rotation>Rotation0</Rotation>\
+												<IsMirrored>False</IsMirrored>\
+												<IsVariable>False</IsVariable>\
+												<ShapeType>HorizontalLine</ShapeType>\
+												<LineWidth>20</LineWidth>\
+												<LineAlignment>LeftOrTop</LineAlignment>\
+												<FillColor Alpha="0" Red="0" Green="0" Blue="0"/>\
+											</ShapeObject>\
+											<Bounds X="307.1997" Y="1050.367" Width="5323.2" Height="94.42623"/>\
+										</ObjectInfo>\
+									</DieCutLabel>';
+
+// ========================================================
 // Shared vars
 // ========================================================
 
@@ -7,6 +157,7 @@ let genreLinks = [...document.querySelectorAll('a[href^="/style/"]')],
 		printers = dymo.label.framework.getPrinters();
 
 // extract the genre names from the genre links
+// returns a string like: 'style / style'
 genreLinks.forEach(function(genre, i) {
 	genres = i < genreLinks.length - 1 ? genres += genre.textContent + ' / ' : genres += genre.textContent;
 });
@@ -56,8 +207,8 @@ function injectPreviewLink() {
 
 /**
  * Sends the label data to the printer for printing
- * @method attachPrintListener
- * @param  {label} label [the label data]
+ * @method attachPreviewListener
+ * @type {string} label [The label data]
  * @return {undefined}
  */
 function attachPrintListener(label) {
@@ -84,14 +235,13 @@ function attachPreviewListener() {
 
 	document.getElementById('label-btn').addEventListener('click', function() {
 
-		let // change this path to point to the label on your local machine
-				path = 'file:///path/to/discogs-label-maker/label/record-label-fields.xml',
-				label = dymo.label.framework.openLabelFile(path),
+		let
 				artist,
 				existingLabel = document.getElementById('preview'),
 				genrePrompt = prompt('Genre?', genres),
 				info,
-				notes = document.querySelector('.notes_text').dataset.content,
+				label = dymo.label.framework.openLabelXml(template),
+				notes = document.querySelector('.notes_text').innerHTML,
 				pngData,
 				release = document.getElementById('profile_title').textContent,
 				title;
@@ -112,7 +262,8 @@ function attachPreviewListener() {
 		artist = info[0].trim();
 		title = info[1].trim();
 
-		// remove/replace emojis
+		// clean up notes
+		notes = notes.replace(/<br>/g, '\n');
 		notes = notes.replace('🎾', '');
 		notes = notes.replace(/⭐️/g, '★');
 
@@ -152,5 +303,5 @@ chrome.extension.sendMessage({}, function() {
 			injectPreviewLink();
 			attachPreviewListener();
 		}
-	}, 10); 
+	}, 10);
 });
